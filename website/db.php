465101af8cedc59daf0172dfe1143ebd59711b7b
tinetrujillo25 <trujillochristinejoy@gmail.com>
2022-07-20 12:25:19 +0800

    Create db.php

---
+++ website/db.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("localhost:3303","root","","dtrmotoworks");
+    if (mysqli_connect_errno()){
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ die();
+ }
+ ?>
